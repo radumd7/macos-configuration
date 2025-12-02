d132c84d7360b8213798dd4bb4df9ccc402f5f74
Radu Moldovan <radu.ioan.moldovan94@gmail.com>
2025-12-02 09:45:48 +0200

    update nvim conf

--- config/nvim/lua/plugins/colorscheme.lua
+++ config/nvim/lua/plugins/colorscheme.lua
@@ -1,69 +1,79 @@
 return {
-  -- add catppuccin theme
-  {
-    "catppuccin/nvim",
-    name = "catppuccin",
-    priority = 1000,
-    init = function()
-      -- Fix for LazyVim bufferline integration (https://github.com/LazyVim/LazyVim/issues/6355)
-      local ok, mod = pcall(require, "catppuccin.groups.integrations.bufferline")
-      if ok and mod and not mod.get and type(mod.get_theme) == "function" then
-        mod.get = mod.get_theme
-      end
-    end,
-    opts = {
-      flavour = "mocha", -- latte, frappe, macchiato, mocha
-      background = { -- :h background
-        light = "latte",
-        dark = "mocha",
-      },
-      transparent_background = false, -- disables setting the background color.
-      show_end_of_buffer = false, -- shows the '~' characters after the end of buffers
-      term_colors = false, -- sets terminal colors (e.g. `g:terminal_color_0`)
-      dim_inactive = {
-        enabled = false, -- dims the background color of inactive window
-        shade = "dark",
-        percentage = 0.15, -- percentage of the shade to apply to the inactive window
-      },
-      no_italic = false, -- Force no italic
-      no_bold = false, -- Force no bold
-      no_underline = false, -- Force no underline
-      styles = { -- Handles the styles of general hi groups (see `:h highlight-args`):
-        comments = { "italic" }, -- Change the style of comments
-        conditionals = { "italic" },
-        loops = {},
-        functions = {},
-        keywords = {},
-        strings = {},
-        variables = {},
-        numbers = {},
-        booleans = {},
-        properties = {},
-        types = {},
-        operators = {},
-      },
-      color_overrides = {},
-      custom_highlights = {},
-      integrations = {
-        cmp = true,
-        gitsigns = true,
-        nvimtree = true,
-        treesitter = true,
-        notify = false,
-        mini = {
-          enabled = true,
-          indentscope_color = "",
-        },
-        -- For more plugins integrations please scroll down (https://github.com/catppuccin/nvim#integrations)
-      },
-    },
-  },
+	-- add catppuccin theme
+	{
+		"catppuccin/nvim",
+		name = "catppuccin",
+		priority = 1000,
+		init = function()
+			-- Fix for LazyVim bufferline integration (https://github.com/LazyVim/LazyVim/issues/6355)
+			local ok, mod = pcall(require, "catppuccin.groups.integrations.bufferline")
+			if ok and mod and not mod.get and type(mod.get_theme) == "function" then
+				mod.get = mod.get_theme
+			end
+		end,
+		opts = {
+			flavour = "mocha", -- latte, frappe, macchiato, mocha
+			background = { -- :h background
+				light = "latte",
+				dark = "mocha",
+			},
+			transparent_background = true, -- disables setting the background color.
+			show_end_of_buffer = false, -- shows the '~' characters after the end of buffers
+			term_colors = false, -- sets terminal colors (e.g. `g:terminal_color_0`)
+			dim_inactive = {
+				enabled = false, -- dims the background color of inactive window
+				shade = "dark",
+				percentage = 0.15, -- percentage of the shade to apply to the inactive window
+			},
+			no_italic = false, -- Force no italic
+			no_bold = false, -- Force no bold
+			no_underline = false, -- Force no underline
+			styles = { -- Handles the styles of general hi groups (see `:h highlight-args`):
+				comments = { "italic" }, -- Change the style of comments
+				conditionals = { "italic" },
+				loops = {},
+				functions = {},
+				keywords = {},
+				strings = {},
+				variables = {},
+				numbers = {},
+				booleans = {},
+				properties = {},
+				types = {},
+				operators = {},
+			},
+			color_overrides = {},
+			custom_highlights = function(colors)
+				return {
+					-- Make snacks.nvim picker transparent
+					SnacksPickerNormal = { bg = "NONE" },
+					SnacksPickerBorder = { bg = "NONE" },
+					SnacksPickerTitle = { bg = "NONE" },
+					-- Also make float windows transparent
+					NormalFloat = { bg = "NONE" },
+					FloatBorder = { bg = "NONE" },
+				}
+			end,
+			integrations = {
+				cmp = true,
+				gitsigns = true,
+				nvimtree = true,
+				treesitter = true,
+				notify = false,
+				mini = {
+					enabled = true,
+					indentscope_color = "",
+				},
+				-- For more plugins integrations please scroll down (https://github.com/catppuccin/nvim#integrations)
+			},
+		},
+	},
 
-  -- Configure LazyVim to load catppuccin
-  {
-    "LazyVim/LazyVim",
-    opts = {
-      colorscheme = "catppuccin",
-    },
-  },
+	-- Configure LazyVim to load catppuccin
+	{
+		"LazyVim/LazyVim",
+		opts = {
+			colorscheme = "catppuccin",
+		},
+	},
 }
